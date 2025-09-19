a1a6cb0bd3259ddc700be5de9656dd61377f9b31
shashwatsinghds <shashwatsingh1@arizona.edu>
2025-09-19 18:05:19 -0500

    Added components to app.route.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { CareerpathComponent } from './components/careerpath/careerpath.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { DownloadsComponent } from './components/downloads/downloads.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'career-path', component: CareerpathComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'downloads', component: DownloadsComponent },
+  { path: '**', redirectTo: '/home' } // Wildcard route for 404
+];
