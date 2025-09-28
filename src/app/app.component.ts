6647e21d581028e580e51d2b7d1c283963c2b434
shashwatsinghds <shashwatsingh1@arizona.edu>
2025-09-27 21:10:23 -0500

    Updated app.coponent to include all components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { CareerPathComponent } from './components/careerpath/careerpath.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { DownloadsComponent } from './components/downloads/downloads.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { BackgroundComponent } from './components/background/background.component';
 
@@ -8,7 +13,17 @@ import { BackgroundComponent } from './components/background/background.componen
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [RouterOutlet, HomeComponent, NavbarComponent, BackgroundComponent],
+  imports: [
+    RouterOutlet, 
+    HomeComponent, 
+    AboutComponent, 
+    PortfolioComponent, 
+    CareerPathComponent, 
+    ContactComponent, 
+    DownloadsComponent,
+    NavbarComponent, 
+    BackgroundComponent
+  ],
   standalone: true
 })
 export class AppComponent {
